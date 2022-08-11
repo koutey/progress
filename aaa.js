5b61d7606a48f656e5a45faaea5d2a1bfab74961
koutey <nicotab45@gmail.com>
2022-08-11 15:28:00 -0300

    aaa

--- aaa.js
+++ aaa.js
@@ -1,3 +1,8 @@
+document.addEventListener("click", redireccion){
+    if  (input === 0;)
+        return{
+            "intente de nuevo"
+        } else{
 function redireccion(){
     location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-}
+}}}
