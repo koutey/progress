824b88f1a8a2b8a3f5edf85f7f5ff3a82b996a3b
koutey <nicotab45@gmail.com>
2022-08-18 20:51:23 -0300

    a

--- aaa.js
+++ aaa.js
@@ -1,3 +1,25 @@
-function redireccion(){
-    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+const nombre = document.getElementById('nombre');
+const contraseña = document.getElementById('contraseña');
+const error = document.getElementById('error');
+error.style.color = 'red';
+
+function  redireccion(){
+    location.href   = 'https://www.youtube.com/watch?v=zAL8i0FzxL8'     
+}
+
+function funcionEnviar(){
+
+    var mensajeError = [];
+
+    if(nombre.value == '' || nombre.value == null){
+        mensajeError.push('ingresa un nombre')
+    }
+    else if(contraseña.value == '' || contraseña.value == null){
+        mensajeError.push('ingresa una contraseña')
+    }   else{
+        redireccion();
+    }
+    error.innerHTML = mensajeError.join(', ');
+
+    return false;
 }
